f75a136223acdbf2e1bd540eacf5bb128b6fbd98
Marco Galassi <marco.galassi6@studio.unibo.it>
2016-06-22 15:53:07 +0200

    minor fix

---
+++ src/aboidsim/controller/AbstractMainLoop.java
@@ -0,0 +1,54 @@
+package aboidsim.controller;
+
+/**
+ * Abstract class.
+ *
+ */
+public abstract class AbstractMainLoop extends Thread implements MainLoop {
+
+	private LoopStatus status;
+	private long fps;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param desiredFps
+	 *            the desired fps.
+	 */
+	public AbstractMainLoop(final long desiredFps) {
+		this.status = LoopStatus.RUNNING;
+		this.fps = desiredFps;
+	}
+
+	@Override
+	public void setStatus(final LoopStatus newStatus) {
+		this.status = newStatus;
+	}
+
+	@Override
+	public LoopStatus getStatus() {
+		return this.status;
+	}
+
+	@Override
+	public void setFPS(final long newFps) {
+		this.fps = newFps;
+	}
+
+	@Override
+	public long getFPS() {
+		return this.fps;
+	}
+
+	@Override
+	public abstract void run();
+
+	@Override
+	public abstract void pauseLoop();
+
+	@Override
+	public abstract void resumeLoop();
+
+	@Override
+	public abstract void abortLoop();
+}
---
+++ src/aboidsim/controller/Application.java
@@ -0,0 +1,24 @@
+package aboidsim.controller;
+
+import aboidsim.view.View;
+
+/**
+ * This class starts the program by declaring Controller and View
+ * implementation.
+ *
+ */
+public class Application {
+
+	private final Controller controller;
+	private final View view;
+
+	/**
+	 * Constructor.
+	 */
+	public Application() {
+		this.controller = null; // TO DO: substitute with actual implementation
+		this.view = null; // TO DO: substitute with actual implementation
+		this.controller.setView(this.view);
+		// this.controller.start();
+	}
+}

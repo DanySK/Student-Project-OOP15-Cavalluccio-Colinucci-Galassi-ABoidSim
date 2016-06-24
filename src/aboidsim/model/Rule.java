903cdb2f582b7f1c283ec07620d047d5a39538d2
Marco Galassi <marco.galassi6@studio.unibo.it>
2016-06-24 17:58:59 +0200

    Rule interface modified

--- src/aboidsim/model/Rule.java
+++ src/aboidsim/model/Rule.java
@@ -15,9 +15,11 @@ public interface Rule {
 	 * This methods apply the rule to determine how a boid should move according
 	 * to the rule.
 	 *
-	 * @param boids
+	 * @param theBoid
+	 *            the boid that will make a decision regarding its movement.
+	 * @param closeBoids
 	 *            the set containing all the nearest boids.
 	 * @return the Vector that describes the boid's movement.
 	 */
-	Vector apply(Set<Boid> boids);
+	Vector apply(final Boid theBoid, final Set<Boid> closeBoids);
 }

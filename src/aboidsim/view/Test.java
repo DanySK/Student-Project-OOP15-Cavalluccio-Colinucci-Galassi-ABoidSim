7e66e3f32e77b8465256439e6edba0cfde844ff5
matteo cavalluzzo <matteo.cavalluzzo@studio.unibo.it>
2016-07-21 19:16:26 +0200

    added class test to test elements of the interface

---
+++ src/aboidsim/view/Test.java
@@ -0,0 +1,36 @@
+package aboidsim.view;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * test class for the elements of the interface.
+ *
+ */
+public class Test extends Application {
+
+    /**
+     * 
+     * */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /**
+     * lo farò successivamente.
+     */
+    public void start(final Stage stage) throws Exception {
+        stage.setTitle("Test");
+
+        final List<String> parole = Arrays.asList("Sony", "Xiaomi", "Huawei");
+        // stage.setScene(new SceltaRegole(parole));
+
+        stage.setScene(new BoidSelection(Arrays.asList("ciao", "come", "va")));
+        // stage.setScene(new SceltaRegole(parole));
+        stage.show();
+    }
+
+}

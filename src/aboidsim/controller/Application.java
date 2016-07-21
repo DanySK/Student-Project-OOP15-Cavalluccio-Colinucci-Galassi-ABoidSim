a3a44e159c2bf2d34311b1767087acf860051b5d
Marco Galassi <marco.galassi6@studio.unibo.it>
2016-07-21 16:07:09 +0200

    Application class modified

--- src/aboidsim/controller/Application.java
+++ src/aboidsim/controller/Application.java
@@ -1,5 +1,6 @@
 package aboidsim.controller;
 
+import aboidsim.model.Model;
 import aboidsim.view.View;
 
 /**
@@ -11,11 +12,13 @@ public class Application {
 
 	private final Controller controller;
 	private final View view;
+	private final Model model;
 
 	/**
 	 * Constructor.
 	 */
 	public Application() {
+		this.model = null; // TO DO: substitute with actual implementation
 		this.controller = null; // TO DO: substitute with actual implementation
 		this.view = null; // TO DO: substitute with actual implementation
 		this.controller.setView(this.view);

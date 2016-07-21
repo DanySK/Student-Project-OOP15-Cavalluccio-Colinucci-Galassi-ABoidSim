9d8fa6639b7e72a211f9afd35d2d164aa3947086
Marco Galassi <marco.galassi6@studio.unibo.it>
2016-07-21 16:01:20 +0200

    Controller interface modified

--- src/aboidsim/controller/Controller.java
+++ src/aboidsim/controller/Controller.java
@@ -1,5 +1,6 @@
 package aboidsim.controller;
 
+import aboidsim.model.Model;
 import aboidsim.view.View;
 
 /**
@@ -23,6 +24,22 @@ public interface Controller {
 	 * @return the View
 	 */
 	View getView();
+
+	/**
+	 * Setter. The method sets the model
+	 *
+	 * @param model
+	 *            the model being set.
+	 *
+	 */
+	void setModel(Model model);
+
+	/**
+	 * Getter. The method return the Model currently used.
+	 *
+	 * @return the Model
+	 */
+	View getModel();
 
 	/**
 	 * The method that starts the loop.

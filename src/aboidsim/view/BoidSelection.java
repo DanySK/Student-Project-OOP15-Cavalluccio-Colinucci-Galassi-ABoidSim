ec973ff2153a8253cf37b47e2f64ffa2f04181d4
matteo cavalluzzo <matteo.cavalluzzo@studio.unibo.it>
2016-07-21 19:08:33 +0200

    created classes to select rules and boid

---
+++ src/aboidsim/view/BoidSelection.java
@@ -0,0 +1,64 @@
+package aboidsim.view;
+
+import java.util.List;
+import java.util.Optional;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+public class BoidSelection extends Scene{
+    
+    private static VBox layout = new VBox(8);
+    private final ComboBox<String> box;
+    private Optional<String> selectedItem = Optional.empty();
+    private List<String> boidList;
+    
+
+    /**
+     * constructor of the class.
+     * 
+     * @param elements
+     */
+    public BoidSelection(final List<String> elements) {
+        super(layout);
+        this.boidList = elements;
+        box = new ComboBox<>();
+        box.setPromptText("seleziona elemento da inserire");
+        final Text title = new Text("Seleziona tipo di elemento da inserire");
+        this.insertElements(elements);
+
+        this.box.setOnAction(e -> this.selectedItem = Optional.of(this.box.getValue()));
+
+        final Button show = new Button("mostra elem selezionato");
+        show.setOnAction(e -> this.printSelectedItem());
+        layout.setAlignment(Pos.CENTER);
+        layout.setPadding(new Insets(8));
+        layout.getChildren().addAll(title, box, show);
+
+    }
+
+    /**
+     * 
+     * @param list of elements (es boids) to insert in the combobox
+     */
+    private void insertElements(final List<String> elem) {
+        elem.stream().forEach(e -> this.box.getItems().add(e));
+    }
+
+    /**
+     * test method
+     */
+    private void printSelectedItem() {
+        if (this.selectedItem.isPresent()) {
+            System.out.println(this.boidList.indexOf(this.selectedItem.get()));
+        } else {
+            System.out.println("nessun elemento selezionato");
+        }
+    }
+
+}
---
+++ src/aboidsim/view/RulesSelection.java
@@ -0,0 +1,62 @@
+package aboidsim.view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+public class RulesSelection extends Scene{
+
+    private final List<String> rules;
+    private final List<CheckBox> boxes = new ArrayList<>();
+    private static VBox layout = new VBox(10);
+    private Button conferma = new Button("Conferma");
+
+    
+    /**
+     * 
+     * @param rules list of the rules
+     */
+    public RulesSelection(final List<String> rules) {
+            super(layout);
+            layout.setPadding(new Insets(10));
+            this.rules = rules;
+            this.rules.stream().forEach(e -> this.boxes.add(new CheckBox(e)));
+            Text titolo = new Text("Seleziona regole da abilitare: ");
+            layout.getChildren().add(titolo);
+            layout.getChildren().addAll(this.boxes);
+            layout.getChildren().add(conferma);
+            
+            conferma.setOnAction(e -> this.printSelectedRules());
+            
+    }
+    
+    
+    /**
+     * 
+     * @return a list of the selected rules
+     */
+    public List<Integer> returnSelectedRules() {
+        final  List<Integer> list = new ArrayList<>();
+        boxes.stream().forEach(e -> {
+            if (e.isSelected()) list.add(this.rules.indexOf(e.getText()));
+        });
+        return list;
+    }
+    
+    /**
+     * test method
+     */
+    private void printSelectedRules() {
+        System.out.println(this.returnSelectedRules().toString());
+    }
+    
+    
+    
+    
+}

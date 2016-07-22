114570d15748088b3dc63dce7fd9c89bfef3be1c
matteo cavalluzzo <matteo.cavalluzzo@studio.unibo.it>
2016-07-22 17:17:41 +0200

    minor fixes

--- src/aboidsim/view/SimulationScreen.java
+++ src/aboidsim/view/SimulationScreen.java
@@ -1,5 +1,18 @@
 package aboidsim.view;
 
-public class SimulationScreen {
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class SimulationScreen extends Scene {
+
+	final Group root = new Group();
+	public SimulationScreen(Parent arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
 
 }

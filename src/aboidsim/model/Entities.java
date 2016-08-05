a77b522f8203e9e2892df576fe42af2f21fc1d91
Marco Galassi <marco.galassi6@studio.unibo.it>
2016-08-05 18:07:27 +0200

    averageSpeeds changed due to tests

--- src/aboidsim/model/Entities.java
+++ src/aboidsim/model/Entities.java
@@ -10,17 +10,17 @@ package aboidsim.model;
 
 enum Entities {
 
-	TREE_L0("TREE", "tree.png", 0, 500, 30, 50, 0), HERBIVORE_L1("HERBIVORE - LEVEL 1", "herbivore0.png", 1, 100,
-			90, 10, 5), HERBIVORE_L2("HERBIVORE - LEVEL 2", "herbivore1.png", 2, 200, 95, 9, 7), HERBIVORE_L3(
-					"HERBIVORE - LEVEL 3", "herbivore2.png", 3, 300, 100, 8,
-					9), HERBIVORE_L4("HERBIVORE - LEVEL 4", "herbivore3.png", 4, 400, 105, 7, 11), HERBIVORE_L5(
+	TREE_L0("TREE", "tree.png", 0, 500, 30, 50, 0), HERBIVORE_L1("HERBIVORE - LEVEL 1", "herbivore0.png", 1, 100, 90,
+			10, 5), HERBIVORE_L2("HERBIVORE - LEVEL 2", "herbivore1.png", 2, 200, 95, 9, 6), HERBIVORE_L3(
+					"HERBIVORE - LEVEL 3", "herbivore2.png", 3, 300, 100, 7,
+					9), HERBIVORE_L4("HERBIVORE - LEVEL 4", "herbivore3.png", 4, 400, 105, 7, 8), HERBIVORE_L5(
 							"HERBIVORE - LEVEL 5", "herbivore4.png", 5, 500, 110, 6,
-							13), PREDATOR_L6("PREDATOR - LEVEL 6", "predator0.png", 6, 600, 115, 5, 15), PREDATOR_L7(
+							9), PREDATOR_L6("PREDATOR - LEVEL 6", "predator0.png", 6, 600, 115, 5, 11), PREDATOR_L7(
 									"PREDATOR - LEVEL 7", "predator1.png", 7, 700, 120, 4,
-									17), PREDATOR_L8("PREDATOR - LEVEL 8", "predator2.png", 8, 800, 125, 3,
-											19), PREDATOR_L9("PREDATOR - LEVEL 9", "predator3.png", 9, 900, 130, 2,
-													21), PREDATOR_L10("PREDATOR - LEVEL 10", "predator4.png", 10, 100,
-															135, 1, 23);
+									12), PREDATOR_L8("PREDATOR - LEVEL 8", "predator2.png", 8, 800, 125, 3,
+											13), PREDATOR_L9("PREDATOR - LEVEL 9", "predator3.png", 9, 900, 130, 2,
+													14), PREDATOR_L10("PREDATOR - LEVEL 10", "predator4.png", 10, 100,
+															135, 1, 15);
 
 	private final String image;
 	private final String name;

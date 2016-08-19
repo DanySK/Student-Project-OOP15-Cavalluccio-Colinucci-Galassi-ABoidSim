aac0efd04d22b7a54028c16d5a85e3a734533ce1
Marco Galassi <marco.galassi6@studio.unibo.it>
2016-08-19 16:42:26 +0200

    minor fix

--- src/aboidsim/view/PresentationWindow.java
+++ src/aboidsim/view/PresentationWindow.java
@@ -22,67 +22,67 @@ import javafx.stage.Stage;
  */
 class PresentationWindow {
 
-    private static final int HEIGHT = 600;
-    private static final int WIDTH = 800;
+	private static final int HEIGHT = 600;
+	private static final int WIDTH = 800;
 
-    /**
-     * constructor of the class. it creates a new window with the explanation of
-     * the program.
-     *
-     */
-    PresentationWindow() {
-        final Stage stage = new Stage();
-        stage.setTitle("aBoidSim Description");
-        stage.initModality(Modality.APPLICATION_MODAL);
-        final StackPane layout = new StackPane();
+	/**
+	 * constructor of the class. it creates a new window with the explanation of
+	 * the program.
+	 *
+	 */
+	PresentationWindow() {
+		final Stage stage = new Stage();
+		stage.setTitle("aBoidSim - description");
+		stage.initModality(Modality.APPLICATION_MODAL);
+		final StackPane layout = new StackPane();
 
-        final VBox vbox = new VBox();
-        vbox.setPadding(new Insets(10));
+		final VBox vbox = new VBox();
+		vbox.setPadding(new Insets(10));
 
-        final Label text = new Label();
-        text.setText(this.readFromFile());
+		final Label text = new Label();
+		text.setText(this.readFromFile());
 
-        final ScrollPane scrollPane = new ScrollPane(text);
+		final ScrollPane scrollPane = new ScrollPane(text);
 
-        final Button confirm = new Button("Ok!");
-        confirm.setOnAction(e -> stage.close());
+		final Button confirm = new Button("Ok!");
+		confirm.setOnAction(e -> stage.close());
 
-        vbox.getChildren().addAll(scrollPane, confirm);
-        vbox.setSpacing(10);
-        vbox.setAlignment(Pos.CENTER);
-        layout.getChildren().addAll(vbox);
-        layout.setId("presentation");
+		vbox.getChildren().addAll(scrollPane, confirm);
+		vbox.setSpacing(10);
+		vbox.setAlignment(Pos.CENTER);
+		layout.getChildren().addAll(vbox);
+		layout.setId("presentation");
 
-        final Scene scene = new Scene(layout, PresentationWindow.WIDTH, PresentationWindow.HEIGHT);
-        scene.getStylesheets().add("style.css");
-        stage.setScene(scene);
-        stage.showAndWait();
-    }
+		final Scene scene = new Scene(layout, PresentationWindow.WIDTH, PresentationWindow.HEIGHT);
+		scene.getStylesheets().add("style.css");
+		stage.setScene(scene);
+		stage.showAndWait();
+	}
 
-    /**
-     * read the content of the description file
-     *
-     * @return the string representing the content of the text file in res
-     */
-    String readFromFile() {
-        final BufferedReader br = new BufferedReader(new InputStreamReader(
-                this.getClass().getResourceAsStream("/description.txt"), Charset.forName("UTF-8")));
-        final StringBuffer text = new StringBuffer();
-        String line;
-        try {
-            while ((line = br.readLine()) != null) {
-                text.append(line);
-                text.append(System.getProperty("line.separator"));
-            }
-        } catch (final IOException e) {
-            System.out.println("error in reading new line");
-        }
-        try {
-            br.close();
-        } catch (final IOException e) {
-            System.out.println("error in closing buffered reader");
-        }
-        return text.toString();
-    }
+	/**
+	 * read the content of the description file
+	 *
+	 * @return the string representing the content of the text file in res
+	 */
+	String readFromFile() {
+		final BufferedReader br = new BufferedReader(new InputStreamReader(
+				this.getClass().getResourceAsStream("/description.txt"), Charset.forName("UTF-8")));
+		final StringBuffer text = new StringBuffer();
+		String line;
+		try {
+			while ((line = br.readLine()) != null) {
+				text.append(line);
+				text.append(System.getProperty("line.separator"));
+			}
+		} catch (final IOException e) {
+			System.out.println("error in reading new line");
+		}
+		try {
+			br.close();
+		} catch (final IOException e) {
+			System.out.println("error in closing buffered reader");
+		}
+		return text.toString();
+	}
 
 }
